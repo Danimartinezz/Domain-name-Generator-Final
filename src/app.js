1cc89b7eee69d8a09a57cc8f9f8700cf87bd2540
Danimartinezz <daniel.martinez094@mymdc.net>
2022-06-14 01:00:35 +0000

    My first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,28 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+document.querySelector("#btn").addEventListener("click", () => {
+  document.querySelector("#name").innerHTML = domainName();
+  });
+  
+  let pronoun = ['the','this','our'];
+  let adj = ['great','big','green'];
+  let noun = ['jogger','dog','tree'];
+      
+  
+  function domainName (){
+      let i=0;
+      let j=0;
+      let k=0;
+      
+      let temp = "";
+      
+      for (let i=0;i<pronoun.length;i++)
+      {
+          for (let j=0;j<adj.length;j++)
+          {
+              for (let k=0;k<noun.length;k++)
+              {
+                  temp += pronoun[i]+adj[j]+noun[k]+".com <br>";
+              }
+          }
+      }
+      return temp;
+  }
